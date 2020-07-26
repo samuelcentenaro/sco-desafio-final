e3c6492eea481ae3b0bcfe1c5a68d911dfa5482d
Samuel Centenaro <samuelcentenaro@hotmail.com>
2020-07-26 17:32:46 -0300

    Ajustado link baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || 'localhost',
+    baseURL: process.env.REACT_APP_BASE_URL || 'https://sco-desafio-final.herokuapp.com',
 });
 
 export default api;
